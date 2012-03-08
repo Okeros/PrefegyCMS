eb6182307425c3b8ccd41ac44e16c83ae8d0ce41
Okyere Adu-Gyamfi <okyere.adugyamfi@gmail.com>
2012-03-08 00:46:09 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prefegycms::Application.config.secret_token = '831660089c4761c99840ca4d8575b29d3254a500a82e819f6c4737719153de40015b64503cfa45f99a641db2bdaa2d9cc2980f1b4aadc9be3912c610257b3aaa'
